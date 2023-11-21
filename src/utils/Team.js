16e62b59e232e5d2a11460113f76129d125679dc
azher ali <azhermurad@gmail.com>
2023-11-21 08:23:13 +0000

    Score_board_team_class

---
+++ src/utils/Team.js
@@ -0,0 +1,24 @@
+class Team {
+    constructor(
+        pos,
+        teamnName,
+        score1,
+        score2,
+        score3,
+        totalPar,
+        teamImage,
+        rowBackgroundColor,
+        teamBackgroundColor
+    ) {
+        this.pos = pos;
+        this.teamName = teamnName;
+        this.score1 = score1;
+        this.score2 = score2;
+        this.score3 = score3;
+        this.totalPar = totalPar;
+        this.teamImage = teamImage;
+        this.rowBackgroundColor = rowBackgroundColor;
+        this.teamBackgroundColor = teamBackgroundColor;
+    }
+}
+export default Team;
