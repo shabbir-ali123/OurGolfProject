04d8bd3b77bfea85be8a04f1d9a94f34fbc478a2
shabbir-ali123 <shabbiralikhan326@gmail.com>
2023-11-22 12:40:18 +0500

    teacher-left col

--- src/pages/TeacherPage.tsx
+++ src/pages/TeacherPage.tsx
@@ -48,7 +48,7 @@ const TeacherProfile: React.FC = () => {
           showTabs={false}
           discription='" ning processes to achieve superior results. "
           '
-          profilePic="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          profilePic="/img/ellipse-131@2x.png"
           name="Cinderella"
         />
         {/* <TeacherDetails /> */}
