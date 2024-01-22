7737e9becf583bc956ee9b6232bd965da315228b
Fayyazdero <derofayyaz61@gmail.com>
2024-01-23 04:11:27 +0500

    comments api

--- src/pages/ReadPost.tsx
+++ src/pages/ReadPost.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import { addPostComment, fetchSinglePosts } from "../utils/fetchPosts";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { API_ENDPOINTS } from "../appConfig";
-import axios from "axios";
+
 interface SinglePostProps {
   posts: any;
   category: string;
   tags: string;
   mediaFile?: any;
   text: string;
+  PostComments: any;
 }
 
 const ReadPost: React.FC = () => {
@@ -18,30 +18,21 @@ const ReadPost: React.FC = () => {
 
   const [formData, setFormData] = useState<any>({
     content: "",
-    postId: postId
+    postId: postId,
   });
-  const [commentData, setCommentData] = useState<any>({
-    userId: 0,
-    content: "",
-    createdAt: "",
-  });
+
   const [singlePost, setSinglePost] = useState<SinglePostProps>();
 
   useEffect(() => {
     fetchSinglePosts(setSinglePost, postId);
-  }, []);
+  }, [postId]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await addPostComment(formData);
-
-      toast.success(response.message);
-      console.log(response, 'rd');
-      setCommentData({
-        content: response.message,
-        createdAt: response.comment.createdAt,
-      });
+      await addPostComment(formData);
+      toast.success("Comment added successfully");
+      fetchSinglePosts(setSinglePost, postId); // Refresh comments
     } catch (error) {
       toast.error("Getting error, please try again");
     }
@@ -95,6 +86,107 @@ const ReadPost: React.FC = () => {
                 Post comment
               </button>
             </div>
+            {singlePost?.PostComments.map((comment: any) => {
+              return (
+                <div key={comment.id} className="py-4">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-4">
+                      <img
+                        className="w-10 h-10"
+                        src="/img/ellipse-11@2x.png"
+                        alt=""
+                      />
+                      <h4 className="inline-flex items-center mr-2 text-sm font-semibold text-gray-900 dark:text-white">
+                        {comment.userId}
+                      </h4>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        {comment.createdAt}
+                      </p>
+                    </div>
+                    <button
+                      id="dropdownComment1Button"
+                      data-dropdown-toggle="dropdownComment1"
+                      className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg dark:text-gray-400 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                      type="button"
+                    >
+                      <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 16 3"
+                      >
+                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                      </svg>
+                      <span className="sr-only">Comment settings</span>
+                    </button>
+                    <div
+                      id="dropdownComment1"
+                      className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600"
+                    >
+                      <ul
+                        className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                        aria-labelledby="dropdownMenuIconHorizontalButton"
+                      >
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Edit
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Remove
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Report
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div>
+                    <p className="p-0 m-0 text-gray-500 dark:text-gray-400">
+                      {comment.content}
+                    </p>
+                    <div className="flex items-center mt-4 space-x-4">
+                      <button
+                        type="button"
+                        className="flex items-center text-sm font-medium text-gray-500 hover:underline dark:text-gray-400"
+                      >
+                        <svg
+                          className="mr-1.5 w-3.5 h-3.5"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 20 18"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                          />
+                        </svg>
+                        Reply
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </form>
         </div>
       </div>
