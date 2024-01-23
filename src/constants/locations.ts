2ab7040432a17c3db8caa4b263f916e65a92fa95
Fayyazdero <derofayyaz61@gmail.com>
2024-01-24 00:15:00 +0500

    selected locations for events

---
+++ src/constants/locations.ts
@@ -0,0 +1,8 @@
+export const locations = [
+  { province: "Kanto", sub_cities: ["Hunza", "Nagar", "Gilgit", "Balti"] },
+  { province: "Kinki", sub_cities: ["Infi", "Sdsd", "Sds", "Sdcscd"] },
+  {
+    province: "Hokkaido",
+    sub_cities: ["Inasdafi", "Ssaddsd", "Sdssda", "Sasddcscd"],
+  },
+];
