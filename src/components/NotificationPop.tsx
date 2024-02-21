d2c29a30f6d9c7d538c2885077e9d1a6da5386dc
Fayyazdero <derofayyaz61@gmail.com>
2024-02-22 00:24:05 +0500

    added context for notifications and notifications functionality

--- src/components/NotificationPop.tsx
+++ src/components/NotificationPop.tsx
@@ -1,133 +1,148 @@
-import { Fragment, useState } from 'react'
-import { Transition } from '@headlessui/react'
-import { InboxIcon } from '@heroicons/react/24/outline'
-import { XMarkIcon } from '@heroicons/react/20/solid'
-import { useNavigate } from 'react-router-dom';
+import { Fragment, useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
+import { InboxIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { useNavigate } from "react-router-dom";
+import { notificationsContextStore } from "../contexts/notification";
 interface NotificationProp {
-    setNotification: any;
+  setNotification: any;
+  notifcations?: any
 }
-export default function NotificationPop({ setNotification }: any) {
-    const [show, setShow] = useState(true)
-    const navigate = useNavigate();
-    const handleButtonClick = () => {
-        setShow(false);
-        setNotification(false);
-        navigate('/notification-page');
-    };
-    return (
-        <>
+export const NotificationPop: React.FC<NotificationProp> = ({setNotification, notifcations}) => {
+    const [show, setShow] = useState(true);
+    // const [notifications, setuNotications] = useState<any>([])
+    const { notifications } = notificationsContextStore();
 
-            <div
-                aria-live="assertive"
-                className=" pointer-events-none left-0 right-[100px] top-[50px] z-[999]  absolute inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
-            >
-                <div className=" flex w-full flex-col items-center space-y-4 sm:items-end ">
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    setShow(false);
+    setNotification(false);
+    navigate("/notification-page");
+  };
 
-                    <Transition
-                        show={show}
-                        as={Fragment}
-                        enter="transform ease-out duration-300 transition"
-                        enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                        enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                            <div className="p-4">
-                                <div className="flex items-start ">
-                                    <div className="flex-shrink-0">
-                                        <InboxIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-                                    </div>
-                                    <div className="ml-3 w-0 flex-1 pt-0.5">
-                                        <p className="text-sm font-medium text-gray-900">Discussion moved</p>
-                                        <p className="mt-1 text-sm text-gray-500">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.
-                                        </p>
-                                        <div className="mt-3 flex space-x-7">
-                                            <button
-                                                type="button"
-                                                className="rounded-md bg-white text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            >
-                                                Undo
-                                            </button>
-                                            <button
-                                                type="button"
-                                                className="rounded-md bg-white text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            >
-                                                Dismiss
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div className="ml-4 flex flex-shrink-0">
-                                        <button
-                                            type="button"
-                                            className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            onClick={() => {
-                                                setShow(false)
-                                                setNotification(false);
-                                            }}
-                                        >
-                                            <span className="sr-only">Close</span>
-                                            <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <div className="flex items-start">
-                                    <div className="flex-shrink-0">
-                                        <InboxIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-                                    </div>
-                                    <div className="ml-3 w-0 flex-1 pt-0.5">
-                                        <p className="text-sm font-medium text-gray-900">Discussion moved</p>
-                                        <p className="mt-1 text-sm text-gray-500">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.
-                                        </p>
-                                        <div className="mt-3 flex space-x-7">
-                                            <button
-                                                type="button"
-                                                className="rounded-md bg-white text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            >
-                                                Undo
-                                            </button>
-                                            <button
-                                                type="button"
-                                                className="rounded-md bg-white text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            >
-                                                Dismiss
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div className="ml-4 flex flex-shrink-0">
-                                        <button
-                                            type="button"
-                                            className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            onClick={() => {
-                                                setShow(false)
-                                                setNotification(false);
-                                            }}
-                                        >
-                                            <span className="sr-only">Close</span>
-                                            <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='mx-6 my-6 ' onClick={handleButtonClick}>
-                            <button className=" hover:text-[#17b3a6] cursor-pointer text-gray font-bold rounded bg-transparent ">
-                        See All
-                    </button>
-                            </div>
-                           
+  return (
+    <>
+      <div
+        aria-live="assertive"
+        className=" pointer-events-none left-0 right-[100px] top-[50px] z-[999]  absolute inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex flex-col items-center w-full space-y-4 sm:items-end">
+          {notifications.map((item: any) => {
+            return (
+              <Transition
+                show={show}
+                as={Fragment}
+                enter="transform ease-out duration-300 transition"
+                enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg pointer-events-auto ring-1 ring-black ring-opacity-5">
+                  <div className="p-4">
+                    <div className="flex items-start ">
+                      <div className="flex-shrink-0">
+                        <InboxIcon
+                          className="w-6 h-6 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="ml-3 w-0 flex-1 pt-0.5">
+                        <p className="text-sm font-medium text-gray-900">
+                          Discussion moved
+                        </p>
+                        <p className="mt-1 text-sm text-gray-500">
+                          {/* {notifications.map((i)} */}
+                        </p>
+                        <div className="flex mt-3 space-x-7">
+                          <button
+                            type="button"
+                            className="text-sm font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Undo
+                          </button>
+                          <button
+                            type="button"
+                            className="text-sm font-medium text-gray-700 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Dismiss
+                          </button>
                         </div>
-
-                    </Transition>
-                 
-
+                      </div>
+                      <div className="flex flex-shrink-0 ml-4">
+                        <button
+                          type="button"
+                          className="inline-flex text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          onClick={() => {
+                            setShow(false);
+                            setNotification(false);
+                          }}
+                        >
+                          <span className="sr-only">Close</span>
+                          <XMarkIcon className="w-5 h-5" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-4">
+                    <div className="flex items-start">
+                      <div className="flex-shrink-0">
+                        <InboxIcon
+                          className="w-6 h-6 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="ml-3 w-0 flex-1 pt-0.5">
+                        <p className="text-sm font-medium text-gray-900">
+                          Discussion moved
+                        </p>
+                        <p className="mt-1 text-sm text-gray-500">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit oluptatum tenetur.
+                        </p>
+                        <div className="flex mt-3 space-x-7">
+                          <button
+                            type="button"
+                            className="text-sm font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Undo
+                          </button>
+                          <button
+                            type="button"
+                            className="text-sm font-medium text-gray-700 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Dismiss
+                          </button>
+                        </div>
+                      </div>
+                      <div className="flex flex-shrink-0 ml-4">
+                        <button
+                          type="button"
+                          className="inline-flex text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          onClick={() => {
+                            setShow(false);
+                            setNotification(false);
+                          }}
+                        >
+                          <span className="sr-only">Close</span>
+                          <XMarkIcon className="w-5 h-5" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  
                 </div>
-
-            </div>
-        </>
-    )
+              </Transition>
+            );
+          })}
+          <div className="mx-6 my-6 " onClick={handleButtonClick}>
+                    <button className=" hover:text-[#17b3a6] cursor-pointer text-gray font-bold rounded bg-transparent ">
+                      See All
+                    </button>
+                  </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -3,19 +3,16 @@ import { Link } from "react-router-dom";
 import { API_ENDPOINTS } from "../appConfig";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from 'react-router-dom';
+import { notificationsContextStore } from "../contexts/notification";
+import { NotificationPop } from "./NotificationPop";
 
-import {
-  BellIcon
-} from "@heroicons/react/24/solid";
-import Example from "./StudentCalSec";
-import NotificationPop from "./NotificationPop";
 export default function ProfileButton() {
+  const { notifications } = notificationsContextStore();
   const { t, i18n } = useTranslation();
-  document.body.dir = i18n.dir();
+
   const languages = {
     en: { displayName: "English" },
-    ja: { displayName: i18n.language === 'en' ? "Japan" : "日本語" },
+    ja: { displayName: i18n.language === "en" ? "Japan" : "日本語" },
   };
 
   const [token, setToken] = useState("");
@@ -23,7 +20,6 @@ export default function ProfileButton() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [notification, setNotification] = useState(false);
 
-  const navigate = useNavigate();
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     if (storedToken) {
@@ -36,36 +32,31 @@ export default function ProfileButton() {
     getUser(userId)
       .then((userData) => {
         setUser(userData.user);
-        localStorage.setItem('user', JSON.stringify(userData.user));
+        localStorage.setItem("user", JSON.stringify(userData.user));
       })
       .catch((error) => {
         console.error("Error:", error);
       });
-
   }, []);
 
-
   const handleDotClick = () => {
-    setDropdownOpen(prevState => !prevState);
+    setDropdownOpen((prevState) => !prevState);
 
     const timeoutId = setTimeout(() => {
       setDropdownOpen(false);
     }, 5000);
 
     return () => clearTimeout(timeoutId);
-
   };
 
   const handleChangeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
   };
 
-
   const getUser = async (userId: any) => {
     try {
       const token = localStorage.getItem("token");
       if (token && token !== "undefined") {
-
         const response = await axios.get(`${API_ENDPOINTS.GET_USER}${userId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -78,15 +69,11 @@ export default function ProfileButton() {
       throw "sdf";
     }
   };
-  const openNotification = () => {
-    setNotification(prevState => !prevState);
 
-  };
   return (
-    <div className=" lg:flex lg:flex-1 lg:justify-end ">
+    <div className=" lg:flex lg:flex-1 lg:justify-end">
       <div className="relative flex-shrink-0 block">
         <div className="flex items-center">
-
           {token && user ? (
             <>
               <div className="flex items-center justify-center ">
@@ -101,23 +88,30 @@ export default function ProfileButton() {
                 <p className="m-0 text-base font-medium text-black group-hover:text-gray-900">
                   {user.nickName ? user.nickName : user.email}
                 </p>
-                <button onClick={() => handleChangeLanguage("ja")} className={i18n.resolvedLanguage === "ja" ? "actives  rounded-l-full p-1" : "rounded-l-full p-1"}>
+                <button
+                  onClick={() => handleChangeLanguage("ja")}
+                  className={
+                    i18n.resolvedLanguage === "ja"
+                      ? "actives  rounded-l-full p-1"
+                      : "rounded-l-full p-1"
+                  }
+                >
                   {languages.ja.displayName}
                 </button>
                 <button
                   onClick={() => handleChangeLanguage("en")}
-                  className={i18n.resolvedLanguage === "en" ? "actives rounded-r-full  p-1" : "rounded-r-full p-1"}
-
+                  className={
+                    i18n.resolvedLanguage === "en"
+                      ? "actives rounded-r-full  p-1"
+                      : "rounded-r-full p-1"
+                  }
                 >
                   {languages.en.displayName}
                 </button>
-
               </div>
             </>
-
           ) : (
             <>
-
               <div className="flex items-center justify-center ">
                 <img
                   onClick={handleDotClick}
@@ -128,24 +122,34 @@ export default function ProfileButton() {
               </div>
               <div className="ml-3">
                 <p className="m-0 text-base font-medium text-white group-hover:text-gray-900">
-                  <Link to="/login-page" className="text-black">Login</Link>
+                  <Link to="/login-page" className="text-black">
+                    Login
+                  </Link>
                 </p>
                 <div>
-                  <button onClick={() => handleChangeLanguage("ja")} className={i18n.resolvedLanguage === "ja" ? "actives rounded-l-full p-1" : "rounded-l-full p-1"}>
+                  <button
+                    onClick={() => handleChangeLanguage("ja")}
+                    className={
+                      i18n.resolvedLanguage === "ja"
+                        ? "actives rounded-l-full p-1"
+                        : "rounded-l-full p-1"
+                    }
+                  >
                     {languages.ja.displayName}
                   </button>
                   <button
                     onClick={() => handleChangeLanguage("en")}
-                    className={i18n.resolvedLanguage === "en" ? "actives rounded-r-full p-1" : "rounded-r-full p-1"}
-
+                    className={
+                      i18n.resolvedLanguage === "en"
+                        ? "actives rounded-r-full p-1"
+                        : "rounded-r-full p-1"
+                    }
                   >
                     {languages.en.displayName}
                   </button>
-
                 </div>
               </div>
             </>
-
           )}
           <div className="relative">
             <button
@@ -266,25 +270,42 @@ export default function ProfileButton() {
               </div>
             )}
           </div>
-          <div onClick={openNotification} className="px-6">
+          <div
+            onClick={() => {
+              setNotification(!notification);
+            }}
+            className="px-6"
+          >
             <span>
               <div className="relative cursor-pointer">
-                <svg className="w-5 h-5 text-teal-600 animate-wiggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M15.585 15.5H5.415A1.65 1.65 0 0 1 4 13a10.526 10.526 0 0 0 1.5-5.415V6.5a4 4 0 0 1 4-4h2a4 4 0 0 1 4 4v1.085c0 1.907.518 3.78 1.5 5.415a1.65 1.65 0 0 1-1.415 2.5zm1.915-11c-.267-.934-.6-1.6-1-2s-1.066-.733-2-1m-10.912 3c.209-.934.512-1.6.912-2s1.096-.733 2.088-1M13 17c-.667 1-1.5 1.5-2.5 1.5S8.667 18 8 17" /></svg>
-                <div className="px-1 bg-teal-500 rounded-full text-center text-white text-sm absolute -top-3 -end-2">
-                  3
-                  <div className="absolute top-0 start-0 rounded-full -z-10 animate-ping bg-teal-200 w-full h-full" ></div>
+                <svg
+                  className="w-5 h-5 text-teal-600 animate-wiggle"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 21 21"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.585 15.5H5.415A1.65 1.65 0 0 1 4 13a10.526 10.526 0 0 0 1.5-5.415V6.5a4 4 0 0 1 4-4h2a4 4 0 0 1 4 4v1.085c0 1.907.518 3.78 1.5 5.415a1.65 1.65 0 0 1-1.415 2.5zm1.915-11c-.267-.934-.6-1.6-1-2s-1.066-.733-2-1m-10.912 3c.209-.934.512-1.6.912-2s1.096-.733 2.088-1M13 17c-.667 1-1.5 1.5-2.5 1.5S8.667 18 8 17"
+                  />
+                </svg>
+                <div className="absolute px-1 text-sm text-center text-white bg-teal-500 rounded-full -top-3 -end-2">
+                  {notifications.length > 0 && notifications.length}
+                  <div className="absolute top-0 w-full h-full bg-teal-200 rounded-full start-0 -z-10 animate-ping"></div>
                 </div>
               </div>
-
-
-
             </span>
           </div>
-
         </div>
       </div>
-      {notification && <NotificationPop setNotification={setNotification} />}
-
+      {notification && (
+        <NotificationPop
+          notifcations={notifications}
+          setNotification={setNotification}
+        />
+      )}
     </div>
   );
 }
