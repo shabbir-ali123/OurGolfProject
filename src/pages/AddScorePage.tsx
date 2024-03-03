b7b9ad347a33dda1b37267979313a844667fadac
Qasim Fida <qasimfida0054@gmail.com>
2024-03-04 03:41:05 +0500

    collect array and sum them

--- src/pages/AddScorePage.tsx
+++ src/pages/AddScorePage.tsx
@@ -1,63 +1,98 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { singleEventContextStore } from '../contexts/EventContext';
-import { singleTeamsContextStore } from '../contexts/TeamContext';
-import Player from '../components/Player';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { singleEventContextStore } from "../contexts/EventContext";
+import { singleTeamsContextStore } from "../contexts/TeamContext";
+import Player from "../components/Player";
 
 interface GolfScoreProps {
   onSaveScores?: (scores: number[]) => void; // Optional, implement if needed
 }
 
 const GolfScoreInput: React.FC<GolfScoreProps> = ({ onSaveScores }) => {
-  const {isCreated, singleEvent} = singleEventContextStore()
-  const hole = singleEvent ? singleEvent?.selectedHoles : [];
-  const newArrayHole = hole?.split(",").map(Number)
+  const { isCreated, singleEvent } = singleEventContextStore();
+  const hole = singleEvent ? singleEvent.selectedHoles : [];
+  const newArrayHole = hole?.split(",").map(Number);
+
+  const p = singleEvent ? singleEvent.shotsPerHoles : [];
+  const par = p?.split(",").map(Number);
 
   const { t, i18n } = useTranslation();
   document.body.dir = i18n.dir();
-  const [scores, setScores] = useState<number[]>(new Array(18).fill(0));
-  const pars = [4, 3, 5, 4, 4, 3, 5, 4, 4, 3, 5, 4, 4, 3, 5, 4, 4, 3]; 
 
-  const handleScoreChange = (index: number, newScore: number) => {
-    const updatedScores = [...scores];
-    updatedScores[index] = newScore;
-    setScores(updatedScores);
+  const holes = Array.from({ length: 18 }, (_, index: number) => index + 1);
+  const [sums, setSums] = useState<{ [key: string]: any }>({});
+
+  const handleInputChange = (
+    userId: string,
+    holeIndex: number,
+    value: number
+  ) => {
+    const key = userId + "-" + holeIndex;
+    const updatedSums = { ...sums };
+
+    // Initialize the array for the userId if it doesn't exist
+    if (!updatedSums[userId]) {
+      updatedSums[userId] = Array.from({ length: holes.length }, () => 0);
+    }
+
+    // Update the value in the array
+    updatedSums[userId][holeIndex] = value;
+
+    setSums(updatedSums);
   };
 
-  const calculateTotalPar = () => pars.reduce((acc, curr) => acc + curr, 0);
+  const filteredSums = Object.fromEntries(
+    Object.entries(sums).map(([userId, scores]) => {
+      const filteredScores = scores.filter((score: any, index: any) =>
+        newArrayHole?.includes(index + 1)
+      ); // Keep only the scores for holes included in newArrayHole
+      return [userId, filteredScores];
+    })
+  );
+  const calculateTotalSelected = () => {
+    const selectedsum: { [userId: string]: number } = {};
+    for (const userId in filteredSums) {
+      const totalScore = filteredSums[userId].reduce(
+        (acc: any, score: any) => acc + score,
+        0
+      );
+      selectedsum[userId] = totalScore;
+    }
 
-  const calculateTotalScore = () => scores.reduce((acc, curr) => acc + curr, 0);
+    return selectedsum;
+  };
+  const selectedHoleSum = calculateTotalSelected();
+  console.log(selectedHoleSum, "sum selected holes");
+  console.log(filteredSums, "filter");
 
-  const multiplyParByThree = () => calculateTotalPar() * 3;
+  console.log(sums);
 
-  const calculateSubtraction = () => multiplyParByThree() - calculateTotalPar();
+  // Calculate the total sum for each user
+  const calculateTotalSum = () => {
+    const totalSums: { [userId: string]: number } = {};
 
-  const calculateHDCP = () => calculateSubtraction() * 0.8;
+    for (const userId in sums) {
+      const totalScore = sums[userId].reduce(
+        (acc: any, score: any) => acc + score,
+        0
+      );
+      totalSums[userId] = totalScore;
+    }
 
-  const holes = Array.from({ length: 18 }, (_, index) => index + 1);
+    return totalSums;
+  };
 
-  const renderScoreInputs = () => scores.map((score, index) => (
-    <td key={`score-${index}`}>
-      <input
-        type="number"
-        min="0"
-        value={score}
-        onChange={(e) => handleScoreChange(index, parseInt(e.target.value))}
-        className="w-10 text-center border border-solid border-[#054a51] bg-white shadow-lg"
-      />
-    </td>
-  ));
-  
-  const {
-    handleSingleTeam,
-    totalJoinedMembers,
-    teamMembers,
-    isLoading,
-    teams,
-  } = singleTeamsContextStore();
+  console.log(calculateTotalSum());
+  // Inside the GolfScoreInput component
+  const totalScores = calculateTotalSum();
+
+  // Then render the total scores for each user
+
+  const totalPar = par?.reduce((acc:any, curr:any) => acc + curr, 0);
+  const { teams } = singleTeamsContextStore();
 
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div className="max-w-7xl mx-auto">
       <div className="flex items-center gap-10">
         <div className="relative w-[90.5px] h-[147.5px]">
           <img
@@ -71,75 +106,91 @@ const GolfScoreInput: React.FC<GolfScoreProps> = ({ onSaveScores }) => {
             src="/img/rectangle-1249@2x.png"
           />
         </div>
-        <h2 className="tracking-[0.04em] leading-[18px] font-semibold  [text-shadow:0px_7px_4px_#17b3a6] text-21xl"> {t('ADD_SCORE')}</h2>
+        <h2 className="tracking-[0.04em] leading-[18px] font-semibold  [text-shadow:0px_7px_4px_#17b3a6] text-21xl">
+          {t("ADD_SCORE")}
+        </h2>
       </div>
-      <p>Scoring Type: <span className='font-bold'>Double Peria</span> </p>
+      <p>
+        Scoring Type: <span className="font-bold">Double Peria</span>{" "}
+      </p>
       <table className="text-sm text-left text-gray-500 dark:text-gray-400 w-full">
         <thead className="bg-[#054a51] shadow-[0px_0px_13px_rgba(0,_0,_0,_0.25)] h-[63px] min-w-[182px] text-white rounded-lg">
           <tr>
             <th className="px-2 py-3 text-center">HOLE</th>
-            {holes.map(hole => <th className='text-center' key={hole}>{hole}</th>)}
-            <th className='text-center px-2 py-3'>Total</th>
-            <th className='text-center px-2 py-3'>HDCP</th>
-            <th className='text-center px-2 py-3'>Net</th>
-           
+            {holes.map((hole) => {
+              const match = newArrayHole?.includes(hole);
+              const bgColor = match ? "bg-red" : "";
+              return (
+                <th className={`text-center px-2 py-3 ${bgColor}`} key={hole}>
+                  {hole}
+                </th>
+              );
+            })}
+
+            <th className="text-center px-2 py-3">Total</th>
+            {/* <th className="text-center px-2 py-3">HDCP</th>
+            <th className="text-center px-2 py-3">Net</th> */}
           </tr>
           <tr>
             <th className="px-2 py-3">PAR</th>
-            {[...Array(18)].map((_, i) => {
-                                const match = newArrayHole?.includes(i + 1);
+        
+  {par?.map((parValue:any, index:any) => (
+    <th key={index} className="text-center px-2 py-3">
+      {parValue}
+    </th>
+  ))}
+<th  className="text-center px-2 py-3">
+  {totalPar}
+</th>
+<>
 
-
-                const bgColor = match ? "bg-red" : "";
-                return (
-                  <th key={i} scope="col" className={`px-2 py-3 ${bgColor }`}>
-                    {i + 1}
-                  </th>
-                );
-              })}
-            <td className='text-center font-bold'>{calculateTotalPar()}</td>
-            <td className='text-center font-bold'>{calculateHDCP().toFixed(2)}</td>
-            <td className='text-center font-bold'>{(calculateTotalScore() - calculateHDCP()).toFixed(2)}</td>
+</>
           </tr>
-          <tr>
-            <th>Score</th>
-            {renderScoreInputs()}
-            <td className='text-center font-bold'>{calculateTotalScore()}</td>
-            <td></td> 
-            <td></td> 
-          </tr>
-          {teams?.map((team: any) => {
-            
-              {
-                return  team.members?.map((member: any, memberIndex: any) => (
-                   <tr className="py-4 pl-4 whitespace-nowrap">
-                    <Player
-                      isCreator={isCreated}
-                      key={memberIndex}
-                      showNumber={false}
-                      enableHover={true}
-                      onEdit={() => {
-                        // setSelectedPlayerNickname(member.nickName);
-                        // setSelectedUserId(member.userId);
-                        // setSelectedTeamName(team.name);
-                        // setEditOpen(true);
-                      }}
-                      onDelete={() => {}}
-                      name={member.nickName}
-                      imageUrl={member.imageUrl}
+          {teams?.map((team: any, teamIndex: number) =>
+            team.members?.map((member: any, memberIndex: number) => (
+              <tr key={memberIndex} className="py-4 pl-4 whitespace-nowrap">
+                <Player
+                  isCreator={isCreated}
+                  key={memberIndex}
+                  showNumber={false}
+                  enableHover={true}
+                  onEdit={() => {
+                    // setSelectedPlayerNickname(member.nickName);
+                    // setSelectedUserId(member.userId);
+                    // setSelectedTeamName(team.name);
+                    // setEditOpen(true);
+                  }}
+                  onDelete={() => {}}
+                  name={member.nickName}
+                  imageUrl={member.imageUrl}
+                />
+                {holes.map((hole, holeIndex: number) => (
+                  <td key={holeIndex}>
+                    <input
+                      type="number"
+                      min="1"
+                      onChange={(e) =>
+                        handleInputChange(
+                          member.nickName,
+                          holeIndex,
+                          parseInt(e.target.value)
+                        )
+                      }
+                      className="w-10 text-center border border-solid border-[#054a51] bg-white shadow-lg"
                     />
-                    {renderScoreInputs()}
-                    <td className='text-center font-bold'>{calculateTotalScore()}</td>
-
-                  </tr>
-                ));
-              }
-            })}
+                  </td>
+                ))}
+                <td className="text-center px-2 py-3">
+                  {totalScores[member.nickName]}
+                </td>
+              </tr>
+            ))
+          )}
         </thead>
       </table>
       <button
         className="mt-4 px-4 py-2 bg-[#17b3a6] text-white rounded hover:bg-blue-700"
-        onClick={() => onSaveScores && onSaveScores(scores)}
+        onClick={() => {}}
       >
         Save Scores
       </button>
