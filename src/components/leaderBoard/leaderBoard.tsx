1759a2f9fe16d7b91e79940935eee40ec604badb
Fayyazdero <derofayyaz61@gmail.com>
2024-04-04 19:26:24 +0500

    leaderboard table

--- src/components/leaderBoard/leaderBoard.tsx
+++ src/components/leaderBoard/leaderBoard.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import { useScoreContext } from "../../contexts/scoreContext";
 import { useParams } from "react-router-dom";
+import { singleEventContextStore } from "../../contexts/eventContext";
 
 const LeaderBoard = (props: {
   title: string;
@@ -16,29 +17,36 @@ const LeaderBoard = (props: {
 };
 
 const LeaderBoardTables: FunctionComponent = () => {
+  const { singleEvent } = singleEventContextStore();
+
   const params = useParams<{ id?: string }>();
-  const { score } = useScoreContext()
+  const { score } = useScoreContext();
   const { t } = useTranslation();
 
-  const paramsFilter = score?.filter((id: any) => id.eventId == Number(params.id));
-  const filteredScores = paramsFilter?.filter((scores:any, index:any, self:any) => {
-    return self.findIndex((s: any) => s.userId === scores.userId) === index;
-});
+  const paramsFilter = score?.filter(
+    (id: any) => id.eventId == Number(params.id)
+  );
+  const filteredScores = paramsFilter?.filter(
+    (scores: any, index: any, self: any) => {
+      return self.findIndex((s: any) => s.userId === scores.userId) === index;
+    }
+  );
 
-return (
+  let shotsPerHoles = singleEvent?.shotsPerHoles?.split(",").map(Number);
+
+  return (
     <div className=" mx-5 w-[300px] md:w-[1200px] md:mx-[80px] ">
       <div className="px-3 overflow-x-auto">
-      <div className="flex gap-4">
-            <img
-              className="w-[57px] h-[103px]"
-              alt=""
-              src="/img/rectangle-1248@2x.png"
-            />
-            <b className="relative left-[-24px] top-[35px] text-17xl text-darkslateblue-300 leading-[18px] [text-shadow:0px_7px_4px_#ccf2fe]">
-              {'Leader Board'}
-            </b>
-          
-          </div>
+        <div className="flex gap-4">
+          <img
+            className="w-[57px] h-[103px]"
+            alt=""
+            src="/img/rectangle-1248@2x.png"
+          />
+          <b className="relative left-[-24px] top-[35px] text-17xl text-darkslateblue-300 leading-[18px] [text-shadow:0px_7px_4px_#ccf2fe]">
+            {"Leader Board"}
+          </b>
+        </div>
         <table className="w-full border-spacing-y-5 ">
           <thead className="text-left text-white ">
             <tr className=" bg-[#054a51] shadow-[0px_0px_13px_rgba(0,_0,_0,_0.25)] h-[63px] min-w-[182px]">
@@ -68,14 +76,24 @@ return (
                 className=" text-[18px] font-medium text-center"
               />
               <LeaderBoard
-                title={'Net Value'}
+                title={"Net Value"}
                 className=" text-[18px] font-medium text-center"
               />
               <LeaderBoard
-                title={'HandiCap Value'}
+                title={"HandiCap Value"}
                 className="text-[18px] font-medium text-center"
               />
-              
+            </tr>
+            <tr className="bg-[#054a51] shadow-[0px_0px_13px_rgba(0,_0,_0,_0.25)] h-[63px] min-w-[182px]">
+              <LeaderBoard
+                title={t("PAR")}
+                className="rounded-s-[3px] font-bold text-[24px] text-start ml-4"
+              />
+              {shotsPerHoles?.map((shot: any) => {
+                return (
+                  <td className="p-2 leading-[20px] text-center ">{shot}</td>
+                );
+              })}
             </tr>
           </thead>
           {filteredScores?.map((scored: any, index: any) => {
@@ -86,24 +104,29 @@ return (
                 <tr className="bg-white shadow-[0px_0px_13px_rgba(0,_0,_0,_0.25)]  h-[69px] font-medium text-black">
                   <td className="whitespace-nowrap tracking-[1.45px] leading-[9.22px] flex items-center justify-between min-w-[182px] rounded-s-[13px]">
                     <div
-                      className="w-[135px] text-[24px] h-[69px] flex items-center justify-center font-normal leading-5 text-white bg-black"
+                      className="w-[135px] text-[24px] h-[69px] px-4 flex items-center justify-center font-normal leading-5 text-white bg-black overflow-hidden"
                       style={{
                         boxShadow: " 0px 0px 5px 0px #CF24EB",
                       }}
                     >
-                      {t("PAR")}
+                      <img
+                        className="h-11 w-11 rounded-full mr-2"
+                        src={scored?.userScoreCard?.imageUrl}
+                        alt="user"
+                      />
+                      <p className="text-[10px]">
+                        {scored.userScoreCard.nickName}
+                      </p>
                     </div>
                   </td>
-                  {
-                    arr?.map((scores:any) => (
-                        <LeaderBoard
-                          key={scores.hole}
-                          title={`${scores}`}
-                          className="text-[18px] font-medium text-center"
-                        />
-                    ))
-                  }
-               
+                  {arr?.map((scores: any) => (
+                    <LeaderBoard
+                      key={scores.hole}
+                      title={`${scores}`}
+                      className="text-[18px] font-medium text-center"
+                    />
+                  ))}
+
                   <LeaderBoard
                     title={scored?.totalScore}
                     className="text-[18px] font-medium text-center "
@@ -116,7 +139,6 @@ return (
                     title={scored?.handiCapValue}
                     className="text-[18px] font-medium text-center "
                   />
-                  
                 </tr>
               </tbody>
             );
