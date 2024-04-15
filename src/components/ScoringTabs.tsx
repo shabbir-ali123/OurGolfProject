10ac978c5b615b26e21bb8e31ecd27a19c127876
shabbbir-ali123 <shabbbiralikhan326@gmail.com>
2024-04-15 20:57:51 +0500

    scoreboard

--- src/components/ScoringTabs.tsx
+++ src/components/ScoringTabs.tsx
@@ -28,13 +28,12 @@ document.body.dir = i18n.dir();
   return (
     <div className="p-2 ">
       <div className="">
-        <div className="flex justify-between gap-2 mx-8 ">
-          <div className="flex gap-2">
+        <div className="flex justify-end gap-2 mx-8 ">
+          <div className="flex justify-end gap-2">
             <button
               className={` ${
                 activeTab === "team"
-                  ? "bg-purple-600 rounded-md p-4 cursor-pointer animate-bounce border-none py-4 px-20 text-white text-lg"
-                  : "bg-[#EEEEEE] rounded-md p-4 cursor-pointer  border-none py-4 px-20 text-purple-600  text-lg font-bold"
+                  ? "bg-[#17b3a6] rounded-md p-4 cursor-pointer animate-bounce border-none py-4 px-20 text-white text-lg" : "bg-[#EEEEEE] rounded-md p-4 cursor-pointer  border-none py-4 px-20 text-[#17b3a6]  text-lg font-bold"
               } px-4 py-2 border rounded-full`}
               onClick={() => handleTabClick("team")}
             >
@@ -43,8 +42,7 @@ document.body.dir = i18n.dir();
             <button
               className={`${
                 activeTab === "individual"
-                  ? "bg-purple-600 rounded-md p-4 cursor-pointer animate-bounce border-none py-4 px-20 text-white text-lg"
-                  : "bg-[#EEEEEE] rounded-md p-4 cursor-pointer  border-none py-4 px-20 text-purple-600  text-lg font-bold"
+                  ? "bg-[#17b3a6] rounded-md p-4 cursor-pointer animate-bounce border-none py-4  text-white text-lg" : "bg-[#EEEEEE] rounded-md p-4 cursor-pointer  border-none py-4  text-[#17b3a6]  text-lg font-bold"
               } px-4 py-2   rounded-full`}
               onClick={() => handleTabClick("individual")}
             >
