f571e82c9a4d5cfa5b94b9c6554c63e4dcaebbf5
Shabbir Ali <shabbiralikhan326@gmail.com>
2024-08-13 19:46:10 +0500

    changes

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,13 +2,13 @@ import ChatWrapper from "../chat/chatWrapper"
 import AllUserChat from "../components/chat/ListUsers"
 import { UsersList } from "../components/chat/UsersList"
 // import ChatApp from "./ChatBox"
-import NewChat from "../components/chat/NewChat"
+import Messages from "../components/chat/Messages"
 
 export const ChatSystem = () => {
     return (
         <div className="max-w-7xl flex items-start mx-auto  rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark" >
             <AllUserChat/>
-              <NewChat/>
+              <Messages/>
                 
         </div>
     )
